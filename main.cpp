ccf6f02c1dc3a64a5764e297ba6fd5bfbf780499
Amirali Sharifian <amiralis@cs-amoeba-n4.cs.surrey.sfu.ca>
2015-08-17 20:50:10 -0700

    Fix freeing up memory in main function

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 
 void initialize(char **str)
 {
-    //*str = (char *)malloc(sizeof(char) * VECTOR_SIZE);
     *str = new char[VECTOR_SIZE];
 }
 
@@ -25,15 +24,25 @@ int popcnt(long comparision)
 int main()
 {
     char **test_str;
+
+    //Generating reference string
     char *str_ref = new char[VECTOR_SIZE];
     generateRandomSequence(str_ref);
 
+    //Measuring size of array needed for saving a string.
+    //For instance if size of string is 40 then we need an arrya with size two or we need two integer to save the string
+    //array_size is size of that array
     int array_size = (VECTOR_SIZE/WORD_SIZE) + 1;
 
+    //Defining vector of strings for testing. STRING_SIZE means size of our dataset.
+    //According to the size of array_size we define second dimension of the array and 
+    //since we are saving our charecters with three bits so last dimension is just 3
+    // str_vector: vector of strings
+    // v_str_ref : converted version of refference string
     long ***str_vector = new long **[STRING_SIZE];
     long **v_str_ref = new long *[array_size];
-    //long **vector2 = new long *[array_size];
 
+    //Returned value of normal version
     long *res = new long[array_size];
     long res2 = 0;
 
@@ -48,7 +57,6 @@ int main()
         for(int i = 0; i < array_size; i ++)
         {
             str_vector[j][i] = new long [BITSIZE];
-            //vector2[i] = new long [BITSIZE];
             res[i] = 0;
         }
     }
@@ -56,31 +64,17 @@ int main()
 
     srand(100);
 
-    //initialize(&str1);
-    //initialize(&str2);
-    //
-    //Initialize strings
     test_str = new char *[STRING_SIZE];
     for(int i = 0; i < STRING_SIZE; i++)
     {
         test_str[i] = new char[VECTOR_SIZE];
     }
-    //str1 = new char[VECTOR_SIZE];
-    //str2 = new char[VECTOR_SIZE];
 
     print(str_ref);
     for(int i = 0; i < STRING_SIZE; i++)
     {
         generateSecondRandomSeq(str_ref, test_str[i]);
-        //print(test_str[i]);
-        //generateRandomSequence(test_str[i]);
-        //print(test_str[i]);
-        //test_str[i] = str1;
     }
-    // generateRandomSequence(str1);
-    // generateRandomSequence(str2);
-    //print(test_str[10]);
-    //print(test_str[15]);
     bitConvert(str_ref, v_str_ref);
     for(int i = 0; i < STRING_SIZE; i++)
     {
@@ -96,32 +90,34 @@ int main()
     {
         final += popcnt(res[i]);
     }
-    //int final = popcnt(res);
-    //printf("%ld\n", res);
     printf("Number of ones:%d\n", final);
     printf("Test: %ld \n", res2);
 
 
-    // BitBlock a,b,c;
-    // a = mvmd<32>::fill4(0,-1,0,-1);
-    // b = mvmd<32>::fill4(5,-1,0,0);
-    // c = simd<32>::add(a,b);
-    // print_register("a",a);
-    // long testt = mvmd<32>::extract<3>(b);
-    // std::cout << testt << std::endl;
-    // print_register("b",b);
-    // print_register("c",c);
-
-    //Releasing memories
-    // delete[] str1;
-    // delete[] str2;
+    //Freeing memory
+    delete[] str_ref;   
     delete[] res;
-    for(int i = 0; i < array_size; i++)
+    for(int i = 0; i < STRING_SIZE; i++)
     {
+        for(int j = 0; j < array_size; j++)
+        {
+            delete[] str_vector[i][j];
+        }
         delete[] str_vector[i];
-        //delete[] vector2[i];
     }
     delete[] str_vector;
+
+    for(int i = 0; i < array_size; i++)
+    {
+        delete[] v_str_ref[i];
+    }
+    delete[] v_str_ref;
+    for(int i = 0; i < STRING_SIZE; i++)
+    {
+        delete[] test_str[i];
+    }
+    delete[] test_str;
+
 
     return 0;
 }
